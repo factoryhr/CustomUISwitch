12876cb9e5649b174a41fc10421c7e57dd0fa16f
Valentin <valentin.saric@factory.hr>
2019-03-21 10:59:49 +0100

    renamed invalid heder file

--- Sources/CustomUISwitch/CustomUISwitch/CustomUISwitch.h
+++ Sources/CustomUISwitch/CustomUISwitch/CustomUISwitch.h
@@ -9,10 +9,10 @@
 #import <UIKit/UIKit.h>
 
 //! Project version number for LookingForMagentoDevsThatWillProvideFunctionalityToThisAwesomeSwitch.
-FOUNDATION_EXPORT double LookingForMagentoDevsThatWillProvideFunctionalityToThisAwesomeSwitchVersionNumber;
+FOUNDATION_EXPORT double CustomUISwitchVersionNumber;
 
 //! Project version string for LookingForMagentoDevsThatWillProvideFunctionalityToThisAwesomeSwitch.
-FOUNDATION_EXPORT const unsigned char LookingForMagentoDevsThatWillProvideFunctionalityToThisAwesomeSwitchVersionString[];
+FOUNDATION_EXPORT const unsigned char CustomUISwitchVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LookingForMagentoDevsThatWillProvideFunctionalityToThisAwesomeSwitch/PublicHeader.h>
 
